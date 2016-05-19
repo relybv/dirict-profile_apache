f00960f9f5e2b8606fd04b15c3e863580c5d8614
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-19 11:09:01 +0000

    resolve package clash

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,6 +7,12 @@ class profile_apache::install {
   # install packages
   ensure_packages( $::profile_apache::packages )
 
+  if ! defined(Package['nfs-common']) {
+    package { 'nfs-common':
+        ensure => installed,
+    }
+  }
+
   class { 'apache':
     default_vhost => false,
     mpm_module    => 'prefork',
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class profile_apache::params {
   $vhost = $::fqdn
   $docroot = '/var/www'
   $ssl_docroot = "${docroot}/ssl"
-  $php_packages = ['pdftk','php5-common','php5-cli','php5-mcrypt','php5-imagick','php5-curl','php5-gd','php5-imap','php5-xsl','php5-xdebug','php5-mysql','libapache2-mod-php5', 'fop', 'nfs-common']
+  $php_packages = ['pdftk','php5-common','php5-cli','php5-mcrypt','php5-imagick','php5-curl','php5-gd','php5-imap','php5-xsl','php5-xdebug','php5-mysql','libapache2-mod-php5', 'fop']
   $monitor_address = $::monitor_address
   $nfs_address = $::nfs_address
   $nfs_mountpoint = '/mnt/templates'
