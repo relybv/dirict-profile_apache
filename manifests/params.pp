79c15b5f31b4e93f5a26d1904935345e1b35e95b
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-19 08:37:22 +0000

    add nfs-common package for nfs client

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class profile_apache::params {
   $vhost = $::fqdn
   $docroot = '/var/www'
   $ssl_docroot = "${docroot}/ssl"
-  $php_packages = ['pdftk','php5-common','php5-cli','php5-mcrypt','php5-imagick','php5-curl','php5-gd','php5-imap','php5-xsl','php5-xdebug','php5-mysql','libapache2-mod-php5', 'fop']
+  $php_packages = ['pdftk','php5-common','php5-cli','php5-mcrypt','php5-imagick','php5-curl','php5-gd','php5-imap','php5-xsl','php5-xdebug','php5-mysql','libapache2-mod-php5', 'fop', 'nfs-common']
   $monitor_address = $::monitor_address
   $nfs_address = $::nfs_address
   case $::osfamily {
