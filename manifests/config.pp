1ab696a7de282657ad10e82bbad7dd8c3210ffac
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-24 18:48:15 +0000

    make override a string

--- manifests/config.pp
+++ manifests/config.pp
@@ -25,11 +25,11 @@ class profile_apache::config {
     ssl             => true,
     directories     => [
       { path     => $profile_apache::docroot,
-        override => [ all ],
+        override => 'all',
         options  => [ 'Indexes','FollowSymLinks','MultiViews' ],
       },
-      { path     => '/usr/lib/cgi-bin',
-        options  => [ '+ExecCGI','-MultiViews','+SymLinksIfOwnerMatch' ],
+      { path    => '/usr/lib/cgi-bin',
+        options => [ '+ExecCGI','-MultiViews','+SymLinksIfOwnerMatch' ],
       },
     ],
   }
