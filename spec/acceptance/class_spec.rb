7fb3ae02d626d9664128dbebb48dd06d2b0c7582
P. Gomersbach <p.gomersbach@rely.nl>
2016-08-24 12:18:42 +0000

    compleet serverspec tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -22,27 +22,92 @@ describe 'profile_apache class' do
       end
     end
 
-  describe host('8.8.8.8') do
-    it { should be_reachable }
-    it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
-  end
+    describe host('8.8.8.8') do
+      it { should be_reachable }
+      it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
+    end
   
-  describe package('apache2') do
-    it { is_expected.to be_installed }
-  end
+    describe package('apache2') do
+      it { is_expected.to be_installed }
+    end
 
-  describe service('apache2') do
-    it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
-  end
+    describe service('apache2') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
 
-  describe port(80) do
-    it { should be_listening }
-  end
+    describe port(80) do
+      it { should be_listening }
+    end
 
-  describe port(443) do
-    it { should be_listening }
-  end
+    describe port(443) do
+      it { should be_listening }
+    end
+
+    describe group('notarisdossier') do
+      it { should exist }
+    end
+
+    describe user('notarisdossier') do
+      it { should belong_to_primary_group 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/.ssh') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/.ssh/authorized_keys') do
+      it { should be_owned_by 'notarisdossier' }
+      it { should be_mode 600 }
+    end
+
+    describe file('/home/notarisdossier/sessions') do
+      it { should be_owned_by 'notarisdossier' }
+      it { should be_mode 777 }
+    end
+
+    describe file('/home/notarisdossier/redirect') do
+      it { should be_owned_by 'notarisdossier' }
+      it { should be_mode 750 }
+    end
+
+    describe file('/home/notarisdossier/application/current') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/config') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/office-templates') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/errors') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/logs') do
+      it { should be_owned_by 'notarisdossier' }
+    end
+
+    describe file('/home/notarisdossier/redirect/index.html') do
+      it { should be_owned_by 'notarisdossier' }
+      it { should be_grouped_into 'www-data' }
+      it { should be_mode 640 }
+      its(:content) { should match /META HTTP-EQUIV="Refresh"/ }
+    end
+
+    describe file('/home/notarisdossier/application/releases/dummy/frontends/office/public/working.html') do
+      it { should be_owned_by 'notarisdossier' }
+      it { should be_grouped_into 'www-data' }
+      it { should be_mode 640 }
+      its(:content) { should match /<!DOCTYPE html>/ }
+    end
 
   end
 end
