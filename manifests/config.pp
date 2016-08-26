7c98ada360011e119864f7431c45958f4a79ca84
P. Gomersbach <p.gomersbach@rely.nl>
2016-08-26 15:15:12 +0000

    add custom_fragment

--- manifests/config.pp
+++ manifests/config.pp
@@ -108,6 +108,7 @@ class profile_apache::config {
     ssl_cert             => $::profile_apache::install::ssl_cert_path,
     ssl_key              => $::profile_apache::install::ssl_key_path,
     ssl_chain            => '/etc/ssl/certs/Comodo_PositiveSSL_bundle.crt',
+    custom_fragment      => 'RewriteIPResetHeader on',
     directories          => [
       { path           => $profile_apache::office_document_root,
         allow_override => [ 'ALL' ],
