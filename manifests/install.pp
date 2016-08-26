3d57fb4b9613d933d4a0d3a256dfc39f09a18889
P. Gomersbach <p.gomersbach@rely.nl>
2016-08-26 15:36:01 +0000

    add dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -104,7 +104,8 @@ class profile_apache::install {
   }
 
   file { '/usr/lib/apache2/modules/mod_myfixip.so':
-    source => 'puppet:///modules/profile_apache/mod_myfixip.so',
+    source  => 'puppet:///modules/profile_apache/mod_myfixip.so',
+    require => Class[ 'apache' ],
   }
 
 }
