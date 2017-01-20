ed6ce4d1d1f6516cc343046f883e145d0bd47b43
P. Gomersbach <p.gomersbach@rely.nl>
2017-01-20 13:03:21 +0000

    add package spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -42,6 +42,8 @@ describe 'profile_apache' do
           it { is_expected.to contain_package('libapache2-mod-php5') }
           it { is_expected.to contain_package('fop') }
           it { is_expected.to contain_package('dnsutils') }
+          it { is_expected.to contain_package('imagemagick') }
+          it { is_expected.to contain_package('curl') }
 
           it { is_expected.to contain_apache__vhost('foo.example.com non-ssl').with( 'ssl' => false ) }
           it { is_expected.to contain_apache__vhost('foo.example.com ssl').with( 'ssl' => true ) }
