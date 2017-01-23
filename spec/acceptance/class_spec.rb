a3d1d08ba4f857033d04c3d3aa297e2335cfb342
P. Gomersbach <p.gomersbach@rely.nl>
2017-01-23 13:28:04 +0000

    save beaker log files

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -26,11 +26,6 @@ describe 'profile_apache class' do
       it { is_expected.to be_installed }
     end
 
-    describe package('ONZIN') do
-      it { is_expected.to be_installed }
-    end
-
-
     describe service('apache2') do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
